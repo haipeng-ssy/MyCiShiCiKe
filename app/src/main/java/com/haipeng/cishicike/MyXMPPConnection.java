2fb38e7298f8b3d678bb9337b2ad21fcfa88dd89
haipeng-syy <sunyiyan123@gmail.com>
2015-01-13 01:04:23 +0800

    xmpp连接测试环境的添加

--- app/src/main/java/com/haipeng/cishicike/MyXMPPConnection.java
+++ app/src/main/java/com/haipeng/cishicike/MyXMPPConnection.java
@@ -19,22 +19,47 @@ public class MyXMPPConnection {
 //    public static final int SERVER_PORT = 5222;
 //    public static final String SERVER_NAME = "google.com";
 
-    public static final String SERVER_HOST = "10.12.49.31";
-    public static final int SERVER_PORT = 5222;
-    public static final String SERVER_NAME = "mds-sunyiyan";
+    //公司office computer
+//    public static final String SERVER_HOST = "10.12.49.31";
+//    public static final int SERVER_PORT = 5222;
+//    public static final String SERVER_NAME = "mds-sunyiyan";
+//
+//    String username = "wang";
+//    String password = "wo83450837";
+//    String resource = "jadder.org";
 
-    String username = "wang";
-    String password = "wo83450837";
+    //宿舍网络，my computer
+//    public static final String SERVER_HOST = "192.168.1.103";
+//    public static final int SERVER_PORT = 5222;
+//    public static final String SERVER_NAME = "3e9ty4tqgbjxh3mr";
+//
+//
+//    String username = "admin";
+//    String password = "admin";
+//    String resource = "jadder.org";
+
+    //google
+    public static final String SERVER_HOST = "gmail.com";
+    public static final int SERVER_PORT = 5222;
+//    public static final String SERVER_NAME = "3e9ty4tqgbjxh3mr";
+
+
+    String username = "admin";
+    String password = "admin";
     String resource = "jadder.org";
+
     ConnectionConfiguration configuration;
     XMPPTCPConnection connection;
 
     public boolean mConnection() {
-        configuration = new ConnectionConfiguration(SERVER_HOST, SERVER_PORT, SERVER_NAME);
+//        configuration = new ConnectionConfiguration(SERVER_HOST, SERVER_PORT, SERVER_NAME);
+
+        //for connect google
+        configuration = new ConnectionConfiguration(SERVER_HOST, SERVER_PORT);
 //    configuration
         connection = new XMPPTCPConnection(configuration);
         try {
-            connection.login(username, password, resource);
+//            connection.login(username, password, resource);
             connection.connect();
         } catch (Exception exception) {
             exception.printStackTrace();
